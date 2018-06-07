bc6b6f1789cb75f5ff7145a3647c93417f5fb0a9
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-07 18:53:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,8 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(names)
   names.each do |array|
-    
+    puts "Hello, my name is #{array}."
+  end 
+  name.each do |array|
+    puts 
 end
