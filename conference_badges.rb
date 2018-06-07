8dac5947a98cd163f3c418c9fa227afb23edc8c8
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-07 19:11:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,11 +6,15 @@ def badge_maker(name)
     puts "Hello, my name is #{name}."
 end   
 
+badge_maker(name)
+
 def batch_badge_creator(attendees)
   attendees.each do |array|
     puts "Hello, my name is #{array}."
   end 
 end 
+
+batch_badge_creator(attendees)
 
 def assign_rooms(attendees)
   counter = 1 
@@ -20,7 +24,11 @@ def assign_rooms(attendees)
   end 
 end
 
+assign_rooms(attendees)
+
 def printer(attendees)
   batch_badge_creator(attendees)
   assign_rooms(attendees)
 end
+
+printer(attendees)
