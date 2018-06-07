eba841f38a91560880bf243fb25487ef77b03a1b
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-07 19:38:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,8 @@ def batch_badge_creator(attendees)
 end 
 
 def assign_rooms(attendees)
-  attendees.map do |array|
-    puts "Hello, #{array}! You'll be assigned to room #{counter}!"
+  attendees.each_with_index.map do |array, index|
+    "Hello, #{array}! You'll be assigned to room #{index + 1}!"
   end 
 end
 
